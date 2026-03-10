5ed7f2f3d77e1893cb0dcf74f9ea4363066094d6
Jim Downie <jd42@sanger.ac.uk>
2026-03-10 13:18:31 +0000

    feat: add EasyLinClustParser class

--- pymmseqs/parsers/__init__.py
+++ pymmseqs/parsers/__init__.py
@@ -1,14 +1,17 @@
 # pymmseqs/parsers/__init__.py
 
 from .createdb_parser import CreateDBParser
+from .createindex_parser import CreateIndexParser
 from .easy_cluster_parser import EasyClusterParser
+from .easy_linclust_parser import EasyLinClustParser
 from .easy_search_parser import EasySearchParser
 from .search_parser import SearchParser
-from .createindex_parser import CreateIndexParser
+
 __all__ = [
     "EasyClusterParser",
+    "EasyLinClustParser",
     "CreateDBParser",
     "EasySearchParser",
     "SearchParser",
-    "CreateIndexParser"
+    "CreateIndexParser",
 ]
